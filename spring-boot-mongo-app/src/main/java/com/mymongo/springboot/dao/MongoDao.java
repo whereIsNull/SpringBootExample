ea83ad668c2ff456453952d5d9999f8866d15277
cristobalcsc <cristobalcsc@gmail.com>
2020-10-08 20:56:45 +0200

    uploadFileExample

---
+++ spring-boot-mongo-app/src/main/java/com/mymongo/springboot/dao/MongoDao.java
@@ -0,0 +1,8 @@
+package com.mymongo.springboot.dao;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class MongoDao {
+
+}
---
+++ spring-boot-mongo-app/src/main/java/com/mymongo/springboot/exceptions/StorageFileNotFoundException.java
@@ -0,0 +1,11 @@
+package com.mymongo.springboot.exceptions;
+
+public class StorageFileNotFoundException extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 109696943504099621L;
+
+
+}
